8c16e92ea8a5942d6147de1f687bc61f08511f1c
maximiliano-stein <maxi_stein23@hotmail.com>
2025-10-05 21:19:33 -0300

    BACK: fix user pass length

--- backend/src/domain/users/entities/user.entity.ts
+++ backend/src/domain/users/entities/user.entity.ts
@@ -19,7 +19,7 @@ export class User {
   @Column({ type: 'varchar', length: USER.EMAIL_LENGTH, unique: true })
   email: string;
 
-  @Column({ type: 'varchar', length: AUTH.BCRYPT_SALT_ROUNDS, select: false })
+  @Column({ type: 'varchar', length: AUTH.MAX_PASSWORD_LENGTH, select: false })
   password: string;
 
   @Column({ enum: UserType })
