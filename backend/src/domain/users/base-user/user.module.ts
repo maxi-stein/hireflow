be8a709005e0acc49e2ea90276e4f3956817a82f
maximiliano-stein <maxi_stein23@hotmail.com>
2025-11-24 15:43:56 -0300

    BACK: delete imports

--- backend/src/domain/users/base-user/user.module.ts
+++ backend/src/domain/users/base-user/user.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GuardsModule } from 'src/domain/auth/guards/guards.module';
-import { EmployeesService } from '../employee/employee.service';
-import { User, Employee, Candidate, Education } from '../entities';
+import { User} from '../entities';
 import { UsersController } from './user.controller';
 import { UsersService } from './user.service';
-import { EducationService } from '../education/education.service';
-import { WorkExperienceService } from '../work-experience/work-experience.service';
-import { WorkExperience } from '../entities/work-experience.entity';
 
 @Module({
   imports: [
