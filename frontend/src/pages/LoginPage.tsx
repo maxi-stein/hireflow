885723d51a4679137ef251c2784a5046a482e73b
maximiliano-stein <maxi_stein23@hotmail.com>
2025-12-15 19:32:55 -0300

    FRONT: fix redirect uppon login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -11,13 +11,13 @@ export const LoginPage: React.FC = () => {
   const user = useAppStore((state) => state.user);
 
   // Retrieve the page the user was trying to access before being redirected to login.
-  const from = (location.state as any)?.from?.pathname || ROUTES.PUBLIC.HOME.path;
+  const from = (location.state as any)?.from?.pathname;
 
   // If the user is already logged in, redirect to the appropriate dashboard.
   if (user) {
-    const pathToRedirect = user.type === "candidate" 
-      ? ROUTES.PUBLIC.JOBS.path 
-      : ROUTES.EMPLOYEE.DASHBOARD.path;
+    const pathToRedirect =
+      from !== ROUTES.PUBLIC.HOME.path ? from : user.type === "candidate"
+        ? ROUTES.PUBLIC.JOBS.path : ROUTES.EMPLOYEE.DASHBOARD.path;
     return <Navigate to={pathToRedirect} replace />;
   }
 
@@ -26,12 +26,13 @@ export const LoginPage: React.FC = () => {
       <Card>
         <LoginForm
           onSuccess={(loggedInUser) => {
+            // Always redirect to previous page if it's not the home page.
+            // Otherwise, redirect to the Jobs page for candidates or the Dashboard for employees.
             const pathToRedirect =
-              loggedInUser.type === "candidate"
-                ? from === ROUTES.PUBLIC.HOME.path
-                  ? ROUTES.PUBLIC.JOBS.path
-                  : from
-                : ROUTES.EMPLOYEE.DASHBOARD.path;
+              from !== ROUTES.PUBLIC.HOME.path ? from :
+                loggedInUser.type === "candidate"
+                  ? ROUTES.PUBLIC.JOBS.path : ROUTES.EMPLOYEE.DASHBOARD.path;
+            console.log("redirijo a: ", pathToRedirect);
             navigate(pathToRedirect, { replace: true });
           }}
         />
