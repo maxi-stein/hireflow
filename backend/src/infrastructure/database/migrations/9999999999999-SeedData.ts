87f8bcf2e6d03839803040053afe60893ad471c9
Maximiliano Stein <maxi_stein23@hotmail.com>
2026-08-16 20:11:31 -0300

    BACK: Fix migration seed data

--- backend/src/infrastructure/database/migrations/9999999999999-SeedData.ts
+++ backend/src/infrastructure/database/migrations/9999999999999-SeedData.ts
@@ -321,8 +321,8 @@ export class SeedData9999999999999 implements MigrationInterface {
       INSERT INTO "user_files" 
         ("id", "file_name", "stored_name", "file_path", "mime_type", "size", "file_type", "candidate_id", "created_at", "updated_at")
       VALUES 
-        ('550e8400-e29b-41d4-a716-44665544001a', '550e8400-e29b-41d4-a716-446655440004.jpg', '550e8400-e29b-41d4-a716-446655440004.jpg', 'uploads/profile_pictures/550e8400-e29b-41d4-a716-446655440004.jpg', 'image/jpeg', 29782, 'profile_picture', '550e8400-e29b-41d4-a716-446655440004', NOW(), NOW()),
-        ('550e8400-e29b-41d4-a716-44665544001c', '550e8400-e29b-41d4-a716-446655440005.jpg', '550e8400-e29b-41d4-a716-446655440005.jpg', 'uploads/profile_pictures/550e8400-e29b-41d4-a716-446655440005.jpg', 'image/jpeg', 29782, 'profile_picture', '550e8400-e29b-41d4-a716-446655440005', NOW(), NOW())
+        ('550e8400-e29b-41d4-a716-44665544001a', '550e8400-e29b-41d4-a716-446655440004.jpg', '550e8400-e29b-41d4-a716-446655440004.jpg', 'uploads/profile_pictures/550e8400-e29b-41d4-a716-446655440004.jpg', 'image/jpeg', 29728, 'profile_picture', '550e8400-e29b-41d4-a716-446655440004', NOW(), NOW()),
+        ('550e8400-e29b-41d4-a716-44665544001c', '550e8400-e29b-41d4-a716-446655440005.jpg', '550e8400-e29b-41d4-a716-446655440005.jpg', 'uploads/profile_pictures/550e8400-e29b-41d4-a716-446655440005.jpg', 'image/jpeg', 42405, 'profile_picture', '550e8400-e29b-41d4-a716-446655440005', NOW(), NOW())
     `);
 
     // Candidate applications
